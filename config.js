dec2892dec95e66d7ea3ecc7b77bfaa07283cfd0
pancha000 <panchamd2512@gmail.com>
2024-11-24 22:52:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhyWYx5_IFWOx3de2MtnBrwxK_r1gcNhwa6w&s",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello. I am SL_PANCHA_MD ☠👋✅ whatsapp bot 💥 i am alive now!💖",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 };
 config.js
 Displaying config.js.
