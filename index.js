0e2bd6ae908eedf80a425604c3c3336b3dd3c8e6
pancha000 <panchamd2512@gmail.com>
2024-11-26 16:53:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '*'
+
 
 const ownerNumber = ['94766087184']
 
@@ -46,6 +46,12 @@ async function connectToWA() {
 const connectDB = require ('./ilb/mongodb')
 connectDB();
 //=========================================================================================================
+const {readEnv} = require('./lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//============================================================================================================
+
+        
 console.log("Connecting SL_PANCHA_MD bot ☠👋...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
