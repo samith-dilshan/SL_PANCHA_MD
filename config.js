e1a0fa10b4ab662f03b01c167f9e69daaa707c6b
pancha000 <panchamd2512@gmail.com>
2024-11-26 16:24:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhyWYx5_IFWOx3de2MtnBrwxK_r1gcNhwa6w&s",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello. I am SL_PANCHA_MD ☠👋✅ whatsapp bot 💥 i am alive now!💖",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
-MODE: process.env.MODE || "private",
+MONGODB: process.env.MONGODB || "enter mongodb here",
 };
 config.js
 Displaying config.js.
