bcbce5633ad4de8ea8a8dabcd5562786a231b5a5
pancha000 <panchamd2512@gmail.com>
2024-11-27 11:29:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "",
 MONGODB: process.env.MONGODB || "enter mongodb here",
 };
 config.js
